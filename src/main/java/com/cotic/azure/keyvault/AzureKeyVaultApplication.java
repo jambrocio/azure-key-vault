02385558ed645b6a2174dd71a6528f3b97766cfb
jambrocio <jc.ambrocio.sernaque@gmail.com>
2025-04-09 16:42:50 -0500

    Initial commit

---
+++ src/main/java/com/cotic/azure/keyvault/AzureKeyVaultApplication.java
@@ -0,0 +1,13 @@
+package com.cotic.azure.keyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureKeyVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureKeyVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cotic/azure/keyvault/azure_key_vault/AzureKeyVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotic.azure.keyvault.azure_key_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureKeyVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
